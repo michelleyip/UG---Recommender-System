a1db58b95ce9891eaab2e764b79c4993dcaae22c
michelleyip <49823321+michelleyip@users.noreply.github.com>
2019-04-23 10:56:12 +0100

    Updated rank.py

--- rank.py
+++ rank.py
@@ -9,14 +9,17 @@ import pandas as pd
 import math
 import scipy.stats as stats
 
+# edit csv file names below
 rating_list = pd.read_csv("rating_45NN.csv", na_values = ['no info', '.'], low_memory=False).head(100)
 no_rating_list = pd.read_csv("rating_20NN.csv", na_values = ['no info', '.'], low_memory=False).head(100)
 x =rating_list['Hotel_Address'].values
 y =no_rating_list['Hotel_Address'].values
 
+# kendall tau
 tau, p_value = stats.kendalltau(x, y)
 print 'tau ', tau
 print 'p ', p_value
 
+# spearman rank
 spearman = stats.spearmanr(x, y)[0]
 print 'spearman ', spearman
